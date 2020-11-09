3d86edd46681b1d4b4a9be0b600d1f0992cf3588
Arezki_Tigmit <arezkitigmit@gmail.com>
2020-11-09 14:10:58 +0100

    addedd catalogue

--- app/src/main/java/com/example/safesoftapplication/ViewModel/CatalogueVM.kt
+++ app/src/main/java/com/example/safesoftapplication/ViewModel/CatalogueVM.kt
@@ -2,20 +2,18 @@ package com.example.safesoftapplication.ViewModel
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.toPublisher
 import androidx.paging.PagedList
+
 import com.example.safesoftapplication.backend.api.bdLocal.entity.ProduitEntity
-import com.example.safesoftapplication.backend.api.bdLocal.repositoryBdLocal.CatalogueRepository
-import java.util.*
+import com.example.safesoftapplication.repository.CatalogueRepository
 
 class CatalogueVM @ViewModelInject constructor(
     private val catalogueRepository: CatalogueRepository,
-    private val config: PagedList.Config
-) :BaseViewModel() {
-
-    val searchQuery = MutableLiveData<String>()
-//    val productsList: LiveData<PagedList<ProduitEntity>> =
-//        catalogueRepository.getAllProduits().toLiveData(config = config)
+//    private val config: PagedList.Config,
+    ) :BaseViewModel() {
 
 
+    fun getAllProduits(query: String): LiveData<List<ProduitEntity>> =
+        catalogueRepository.getAllProduits()
 }
