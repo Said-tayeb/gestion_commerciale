84c36f18a5654f3483452c747b9958c44c821a9f
Arezki_Tigmit <arezkitigmit@gmail.com>
2020-11-09 14:28:56 +0100

    arezki

--- app/src/main/java/com/example/safesoftapplication/ViewModel/CatalogueVM.kt
+++ app/src/main/java/com/example/safesoftapplication/ViewModel/CatalogueVM.kt
@@ -1,17 +1,12 @@
 package com.example.safesoftapplication.ViewModel
 
 import androidx.hilt.lifecycle.ViewModelInject
-<<<<<<< HEAD
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.toPublisher
-import androidx.paging.PagedList
+
 
 import com.example.safesoftapplication.backend.api.bdLocal.entity.ProduitEntity
 import com.example.safesoftapplication.repository.CatalogueRepository
-=======
-import androidx.lifecycle.MutableLiveData
-import androidx.paging.PagedList
->>>>>>> 87fd40798f2fd3a3db213f7a87dc0c4b77039567
+
 
 class CatalogueVM @ViewModelInject constructor(
     private val catalogueRepository: CatalogueRepository,
--- app/src/main/java/com/example/safesoftapplication/repository/CatalogueRepository.kt
+++ app/src/main/java/com/example/safesoftapplication/repository/CatalogueRepository.kt
@@ -3,15 +3,8 @@ package com.example.safesoftapplication.repository
 import com.example.safesoftapplication.backend.api.bdLocal.dao.CatalogueDao
 import javax.inject.Inject
 
-<<<<<<< HEAD:app/src/main/java/com/example/safesoftapplication/repository/CatalogueRepository.kt
+
 class CatalogueRepository @Inject constructor(private val catalogueDao: CatalogueDao) {
 
     fun getAllProduits() = catalogueDao.getAllProduits()
 }
-=======
-//
-//class CatalogueRepository @Inject constructor(private val catalogueDao: CatalogueDao){
-//
-//    fun getAllProduits()= catalogueDao.getAllProduits()
-//}
->>>>>>> 87fd40798f2fd3a3db213f7a87dc0c4b77039567:app/src/main/java/com/example/safesoftapplication/backend/api/bdLocal/repositoryBdLocal/CatalogueRepository.kt
